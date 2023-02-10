391e57ab3cf0169edcf82f1ab18fc599d9fe745b
Svetlana Arutsiunova <ys20051980@gmail.com>
2023-02-10 09:57:29 +0300

    uslovie

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "marta")
+{
+    Console.WriteLine("Ura, eto Marta!");
+}
+
+else 
+{
+    Console.WriteLine("Hello, ");
+    Console.WriteLine(username);
+}
+
