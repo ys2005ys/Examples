678f4e0da6d963250795010b60c2763c00bbbbca
Svetlana Arutsiunova <ys20051980@gmail.com>
2023-02-12 17:23:35 +0300

    add 10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {12, 13, 14, 4, 15, 4, 16, 17, 18, 19, 20};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+ {
+    if (array[index] == find)
+    { 
+        Console.WriteLine(index);
+        break;    
+    }
+
+    // index= index+1
+    index++;
+ }
